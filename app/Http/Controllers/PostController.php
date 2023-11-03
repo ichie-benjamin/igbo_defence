504a9f6dde6a25e97399e14bc023b1b702c9e51f
benny <benjaminchukwudi0@gmail.com>
2023-11-03 16:58:22 +0100

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index(){
 //        $software = $request->get('software_id');
         return view('dashboard.posts.index', [
-            'data' => SpladeTable::for(Post::published())
+            'data' => SpladeTable::for(Post::published()->latest())
                 ->withGlobalSearch(columns: ['post_title','post_excerpt'])
                 ->defaultSort('post_title')
                 ->column(key: 'post_title', sortable: true)
